17fdae3653ae4f2ea08b6999c0fdc159512b0753
wjdghks <wjdghks728@naver.com>
2023-08-20 17:13:16 +0900

    [Test] then 수정

--- src/test/java/com/pu/programus/location/LocationControllerTest.java
+++ src/test/java/com/pu/programus/location/LocationControllerTest.java
@@ -1,29 +1,50 @@
 package com.pu.programus.location;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
-class LocationControllerTest {
-    @Autowired
-    private LocationController locationController;
-    @Test
-    void get_all_location_test() {
-        // given
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(LocationController.class)
+class LocationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LocationService service;
+
+    @MockBean
+    private LocationRepository repository;
+
+    @Test
+    void getAllLocationShouldReturnLocationsFromService() throws Exception {
+        // given
+        Location seoul = Location.builder().name("서울").build();
+        Location pusan = Location.builder().name("부산").build();
+        Location daegu = Location.builder().name("대구").build();
+        repository.save(seoul);
+        repository.save(pusan);
+        repository.save(daegu);
+
+        List<Location> locations = new ArrayList<>(List.of(seoul, pusan, daegu));
         // when
+        when(service.getAllLocation()).thenReturn(locations);
 
         // then
-        Assertions.assertThat(locationController).isNotNull();
+        this.mockMvc.perform(get("/location")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("['서울', '부산', '대구']"));
     }
 
 }
