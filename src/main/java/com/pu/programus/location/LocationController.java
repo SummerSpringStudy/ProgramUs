39a0a8661308ae159fb27e2b9adb96cdaec0fa67
wjdghks <wjdghks728@naver.com>
2023-08-20 17:12:34 +0900

    [Modify] List<LocationDTO> 반환

--- src/main/java/com/pu/programus/location/LocationController.java
+++ src/main/java/com/pu/programus/location/LocationController.java
@@ -1,5 +1,6 @@
 package com.pu.programus.location;
 
+import com.pu.programus.location.DTO.LocationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class LocationController {
@@ -16,11 +18,14 @@ public class LocationController {
     /**
      * 모든 지역 정보를 가져오는 API
      *
-     * @return ResponseEntity< List < Location > > 200 OK, 지역 정보 목록
+     * @return ResponseEntity<List < Location> > 200 OK, 지역 정보 목록
      */
     @GetMapping("/location")
-    public ResponseEntity<List<Location>> getAllLocation() {
+    public ResponseEntity<List<LocationDTO>> getAllLocation() {
         List<Location> locationList = locationService.getAllLocation();
-        return ResponseEntity.status(HttpStatus.OK).body(locationList);
+        List<LocationDTO> result = locationList.stream()
+                .map(e -> LocationDTO.builder().name(e.getName()).build())
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
