0b09cc298137783a26795b4226aa6965676b0ae4
kikuke <kkkjs0814@naver.com>
2023-08-22 16:09:23 +0900

    [Add] PositionController

--- src/main/java/com/pu/programus/position/Position.java
+++ src/main/java/com/pu/programus/position/Position.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @Entity
 @Data
+@Builder
 public class Position {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/pu/programus/position/PositionService.java
+++ src/main/java/com/pu/programus/position/PositionService.java
@@ -1,5 +1,6 @@
 package com.pu.programus.position;
 
+import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,17 @@ public class PositionService {
     public List<Position> getPositionList() {
         List<Position> positions = positionRepository.findAll();
         return positions;
+    }
+
+    public void addPosition(String positionName) {
+        Position position = Position.builder()
+                .name(positionName)
+                .build();
+
+        Position savedPosition = positionRepository.save(position);
+        // Todo: 새로 익셉션 만들기?
+        if (savedPosition == null)
+            throw new IOException("저장에 실패했습니다.");
     }
     
     //포지션 기준으로 프로젝트 조회
