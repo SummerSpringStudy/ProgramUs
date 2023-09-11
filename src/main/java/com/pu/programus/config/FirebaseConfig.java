489a09e0c0e3066902b734b47245fa17cd8c46ae
wjdghks <wjdghks728@naver.com>
2023-09-11 22:22:56 +0900

    코드 정리

--- src/main/java/com/pu/programus/config/FirebaseConfig.java
+++ src/main/java/com/pu/programus/config/FirebaseConfig.java
@@ -1,18 +1,11 @@
 package com.pu.programus.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Bucket;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.cloud.StorageClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -41,25 +34,4 @@ public class FirebaseConfig {
             log.error("Firebase init failed" + e.getMessage());
         }
     }
-    /*@Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream inputStream = new FileInputStream("src/main/resources/private/storageKey.json");
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(inputStream))
-                .setStorageBucket("programus-11060.appspot.com/")
-                .build();
-
-        return FirebaseApp.initializeApp(options);
-    }
-
-    @Bean
-    public FirebaseAuth firebaseAuth() throws IOException {
-        return FirebaseAuth.getInstance(firebaseApp());
-    }
-
-    @Bean
-    public Bucket bucket() throws IOException {
-        return StorageClient.getInstance(firebaseApp()).bucket();
-    }*/
 }
